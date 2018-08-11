7b86a8b6088c55a7f56c63d61d5695d3996e1295
pq <pq.blogspot@gmail.com>
2018-08-12 00:46:47 +0900

    HM

--- myRs2/src/Scripts/python/pythonpath/indoc/exceptiondialog.py
+++ myRs2/src/Scripts/python/pythonpath/indoc/exceptiondialog.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # import pydevd; pydevd.settrace(stdoutToServer=True, stderrToServer=True)
 import os, platform, subprocess, traceback, unohelper
-from indoc import dialogcommons
 from com.sun.star.awt import MessageBoxButtons, MessageBoxResults  # 定数
 from com.sun.star.awt.MessageBoxType import ERRORBOX, QUERYBOX  # enum
 from com.sun.star.util import URL  # Struct
@@ -71,3 +70,65 @@ def createDialog(xscriptcontext):
 					os.system('start "" "{}" "{}:{}"'.format(geanypath, filepath, lineno))  # バックグランドでGeanyでカーソルの行番号を指定して開く。第一引数の""はウィンドウタイトル。
 				else:
 					os.system("{} {}:{} &".format(geanypath, filepath, lineno))  # バックグランドでGeanyでカーソルの行番号を指定して開く。
+					
+def dialogCreator(ctx, smgr, dialogprops):  # ダイアログと、それにコントロールを追加する関数を返す。まずダイアログモデルのプロパティを取得。
+	dialog = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialog", ctx)  # ダイアログの生成。
+	if "PosSize" in dialogprops:  # コントロールモデルのプロパティの辞書にPosSizeキーがあるときはピクセル単位でコントロールに設定をする。
+		dialog.setPosSize(dialogprops.pop("PositionX"), dialogprops.pop("PositionY"), dialogprops.pop("Width"), dialogprops.pop("Height"), dialogprops.pop("PosSize"))  # ダイアログモデルのプロパティで設定すると単位がMapAppになってしまうのでコントロールに設定。
+	dialogmodel = smgr.createInstanceWithContext("com.sun.star.awt.UnoControlDialogModel", ctx)  # ダイアログモデルの生成。
+	dialogmodel.setPropertyValues(tuple(dialogprops.keys()), tuple(dialogprops.values()))  # ダイアログモデルのプロパティを設定。
+	dialog.setModel(dialogmodel)  # ダイアログにダイアログモデルを設定。
+	dialog.setVisible(False)  # 描画中のものを表示しない。
+	def addControl(controltype, props, attrs=None):  # props: コントロールモデルのプロパティ、attr: コントロールの属性。
+		control = None
+		items, currentitemid = None, None
+		if controltype == "Roadmap":  # Roadmapコントロールのとき、Itemsはダイアログモデルに追加してから設定する。そのときはCurrentItemIDもあとで設定する。
+			if "Items" in props:  # Itemsはダイアログモデルに追加されてから設定する。
+				items = props.pop("Items")
+				if "CurrentItemID" in props:  # CurrentItemIDはItemsを追加されてから設定する。
+					currentitemid = props.pop("CurrentItemID")
+		if "PosSize" in props:  # コントロールモデルのプロパティの辞書にPosSizeキーがあるときはピクセル単位でコントロールに設定をする。
+			control = smgr.createInstanceWithContext("com.sun.star.awt.UnoControl{}".format(controltype), ctx)  # コントロールを生成。
+			control.setPosSize(props.pop("PositionX"), props.pop("PositionY"), props.pop("Width"), props.pop("Height"), props.pop("PosSize"))  # ピクセルで指定するために位置座標と大きさだけコントロールで設定。
+			controlmodel = _createControlModel(controltype, props)  # コントロールモデルの生成。
+			control.setModel(controlmodel)  # コントロールにコントロールモデルを設定。
+			dialog.addControl(props["Name"], control)  # コントロールをコントロールコンテナに追加。
+		else:  # Map AppFont (ma)のときはダイアログモデルにモデルを追加しないと正しくピクセルに変換されない。
+			controlmodel = _createControlModel(controltype, props)  # コントロールモデルの生成。
+			dialogmodel.insertByName(props["Name"], controlmodel)  # ダイアログモデルにモデルを追加するだけでコントロールも作成される。
+		if items is not None:  # コントロールに追加されたRoadmapモデルにしかRoadmapアイテムは追加できない。
+			for i, j in enumerate(items):  # 各Roadmapアイテムについて
+				item = controlmodel.createInstance()
+				item.setPropertyValues(("Label", "Enabled"), j)
+				controlmodel.insertByIndex(i, item)  # IDは0から整数が自動追加される
+			if currentitemid is not None:  #Roadmapアイテムを追加するとそれがCurrentItemIDになるので、Roadmapアイテムを追加してからCurrentIDを設定する。
+				controlmodel.setPropertyValue("CurrentItemID", currentitemid)
+		if control is None:  # コントロールがまだインスタンス化されていないとき
+			control = dialog.getControl(props["Name"])  # コントロールコンテナに追加された後のコントロールを取得。
+		if attrs is not None:  # Dialogに追加したあとでないと各コントロールへの属性は追加できない。
+			for key, val in attrs.items():  # メソッドの引数がないときはvalをNoneにしている。
+				if val is None:
+					getattr(control, key)()
+				else:
+					getattr(control, key)(val)
+		return control  # 追加したコントロールを返す。
+	def _createControlModel(controltype, props):  # コントロールモデルの生成。
+		if not "Name" in props:
+			props["Name"] = _generateSequentialName(controltype)  # Nameがpropsになければ通し番号名を生成。
+		controlmodel = dialogmodel.createInstance("com.sun.star.awt.UnoControl{}Model".format(controltype))  # コントロールモデルを生成。UnoControlDialogElementサービスのためにUnoControlDialogModelからの作成が必要。
+		if props:
+			values = props.values()  # プロパティの値がタプルの時にsetProperties()でエラーが出るのでその対応が必要。
+			if any(map(isinstance, values, [tuple]*len(values))):
+				[setattr(controlmodel, key, val) for key, val in props.items()]  # valはリストでもタプルでも対応可能。XMultiPropertySetのsetPropertyValues()では[]anyと判断されてタプルも使えない。
+			else:
+				controlmodel.setPropertyValues(tuple(props.keys()), tuple(values))
+		return controlmodel
+	def _generateSequentialName(controltype):  # コントロールの連番名の作成。
+		i = 1
+		flg = True
+		while flg:
+			name = "{}{}".format(controltype, i)
+			flg = dialog.getControl(name)  # 同名のコントロールの有無を判断。
+			i += 1
+		return name
+	return dialog, addControl  # コントロールコンテナとそのコントロールコンテナにコントロールを追加する関数を返す。
